1abb3e5836ace3006c053de5697359e690c34a76
Alexander Beley <beleysasha@gmail.com>
2024-04-14 18:07:43 +1200

    instgrapi testing

--- services/instagram.py
+++ services/instagram.py
@@ -7,8 +7,10 @@ cl = Client()
 def get_video_il(url):
     if 'reel' in url:
         id = cl.media_pk_from_url(url)
-        video_url = cl.media_info(id).video_url
-        cl.video_download_by_url(video_url, folder='/video')
+        media_info = cl.media_info(id)
+        video_url = media_info.video_url
+        # video_url = cl.media_info(id).video_url
+        # cl.video_download_by_url(video_url, folder='/video')
         return video_url
     else:
         print(f'Это не Reel')
@@ -19,4 +21,4 @@ while True:
     time1 = datetime.now()
     video_url = get_video_il(url)
     time2 = datetime.now()
-    print(f'Video url: {video_url}\nDownloaded by {time2 - time1} msec')
+    print(f'Video url: {video_url}\nDownloaded by {time2 - time1} sec')
