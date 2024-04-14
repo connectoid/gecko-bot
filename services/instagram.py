749159c67e3b0b714e0cc2922af95098719e2e33
Alexander Beley <beleysasha@gmail.com>
2024-04-14 14:06:04 +1200

    initial commit

---
+++ services/instagram.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+import instaloader
+
+SHORTCODE = 'C3mXyHpOsiE'
+url = 'https://www.instagram.com/reel/C3rc1NFMb-K/?igsh=MWwxY3czeXByeXVvaw=='
+
+L = instaloader.Instaloader()
+
+def get_video_il(url):
+    if 'reel' in url:
+        SHORTCODE = url.split('/reel/')[1].split('/')[0]
+        post = instaloader.Post.from_shortcode(L.context, SHORTCODE)
+        L.download_post(post, target='video')
+    else:
+        print(f'Это не Reel')
+
+
+get_video_il(url)
