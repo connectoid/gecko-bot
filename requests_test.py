fdec66651d127f5e145a858e91520494bb229ae8
Alexander Beley <beleysasha@gmail.com>
2024-04-16 23:36:25 +1200

    test added

--- requests_test.py
+++ requests_test.py
@@ -70,7 +70,7 @@ success_count = fail_count = 0
 for count in range(1, 101):
     url = random.choice(urls)
     shortcode = url.split('/reel/')[1].split('/')[0]
-    interval = random.randint(3, 4)
+    interval = random.randint(1, 2)
     video_url = get_video_requests(shortcode)
     if video_url:
         print(f'{count}. Ссылка {video_url[:50]} получена удачно. Ждем {interval} сек.')
